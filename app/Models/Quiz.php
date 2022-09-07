864e0bcfb40d94e8e89511cfbc7e46e02ce924a8
Md.Saiful Islam <adeptsaiful@gmail.com>
2022-09-07 12:18:50 +0600

    update

---
+++ app/Models/Quiz.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    use HasFactory;
+
+    const ACTIVE=1;
+    const INACTIVE=0;
+    protected  $table='quizzes';
+    protected $fillable = [
+        'quiz_question',
+        'quiz_topic',
+        'quiz_detail',
+        'option1',
+        'option2',
+        'option3',
+        'option4',
+        'correct_option',
+        'correct_mark',
+        'status',
+        'created_by',
+        'updated_by',
+    ];
+
+ public $quizTopics=['Bangla'=>'Bangla','English'=>'English','Math'=>'Math','Bangladesh'=>'Bangladesh'];
+
+
+}
---
+++ database/migrations/2022_09_07_042955_create_quizzes_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuizzesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->string('quiz_question');
+            $table->text('quiz_topic')->nullable();
+            $table->text('quiz_detail')->nullable();
+
+            $table->string('option1',150)->nullable();
+            $table->string('option2',150)->nullable();
+            $table->string('option3',150)->nullable();
+            $table->string('option4',150)->nullable();
+            $table->string('correct_option',150)->nullable()->comment('Must be option value');
+            $table->tinyInteger('correct_mark',false,3)->default(0);
+            $table->tinyInteger('status')->default(\App\Models\Quiz::ACTIVE);
+
+            $table->unsignedBigInteger('created_by', false);
+            $table->unsignedBigInteger('updated_by', false)->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->cascadeOnDelete();
+            $table->foreign('updated_by')->references('id')->on('users')->cascadeOnDelete();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('quizzes',function (Blueprint $table){
+
+            $table->dropForeign(['created_by']);
+            $table->dropForeign(['updated_by']);
+        });
+
+        Schema::dropIfExists('quizzes');
+    }
+}
